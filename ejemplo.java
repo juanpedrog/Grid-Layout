58761e174b46a94d13f0e9fca7299d86c4fbe158
juanpedrog <juanpedrogilllanos@hotmail.com>
2016-02-02 21:36:50 -0600

    primera version

--- ejemplo.java
+++ ejemplo.java
@@ -14,6 +14,7 @@ public class ejemplo{
 	JButton bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0;
 	JTextField pantalla;
 	public ejemplo(){
+
 		construyePanelSuperior();
 		construyePanelInferior();
 		construyeVentana();
